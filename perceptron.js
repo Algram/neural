7f9e7ed36624e3e7498a13db40ecc98c8aa46af4
Algram <aliasgram@gmail.com>
2016-02-22 22:07:49 +0100

    Added feedForward

--- perceptron.js
+++ perceptron.js
@@ -5,15 +5,28 @@ class Perceptron {
 		this.weights = [];
 
 		for(let i = 0; i < numberOfInputs; i++) {
-      this.weights = this.randomIntFromInterval(-1, 1);
+      this.weights[i] = this.randomIntFromInterval(-1, 1);
+    }
+	}
+
+  feedForward(inputs) {
+    let sum = 0;
+
+    for(let i = 0; i < this.weights.length; i++) {
+      sum += inputs[i] * this.weights[i];
     }
 
-    console.log(this.weights);
-	}
+    return this.activate(sum);
+  }
+
+  activate(sum) {
+    if (sum > 0) return 1;
+    else return -1;
+  }
 
   randomIntFromInterval(min,max) {
     return Math.floor(Math.random()*(max-min+1)+min);
   }
 }
 
-module.exports = Perceptron;
+exports.Perceptron = Perceptron;
