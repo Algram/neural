80ecab0425c6b573091b560b9c681527f4dc9486
Algram <aliasgram@gmail.com>
2016-02-22 22:29:25 +0100

    Added training

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 'use strict';
 
-
 const p = require('./perceptron');
+const t = require('./trainer');
 
-let ptron = new p.Perceptron(3);
+let training = [];
+let trainingLength = 2000;
+
+let minPoint = 500;
+let maxPoint = 500;
+let ptron;
+
+initialize();
+function initialize() {
+  ptron = new p.Perceptron(3);
+
+  for (let i = 0; i < trainingLength; i++) {
+    let x = randomIntFromInterval(minPoint, maxPoint);
+    let y = randomIntFromInterval(minPoint, maxPoint);
+
+    let answer = 1;
+    if (y < f(x)) answer = -1;
+
+    training[i] = new t.Trainer(x, y, answer);
+  }
+}
+
+function f(x) {
+  return 2*x+1;
+}
+
+function randomIntFromInterval(min,max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
